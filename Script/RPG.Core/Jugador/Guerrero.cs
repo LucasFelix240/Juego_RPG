b71ce089c04e1db922056c42fde7dfbe562f121c
LucasFelix240 <lucas.felix.et12@gmail.com>
2022-11-11 17:26:44 -0300

    Actualización v4.0

--- Script/RPG.Core/Jugador/Guerrero.cs
+++ Script/RPG.Core/Jugador/Guerrero.cs
@@ -2,13 +2,14 @@ namespace RPG.Core.Jugador
 {
     public class Guerrero : Personaje
     {
-        
+
         public Guerrero(string unNombre, byte unaVida, short unVelAtaqueBase, short unAtaqueBase) : base(unNombre, unaVida, unVelAtaqueBase, unAtaqueBase)
-        {            
+        {
         }
-
         public override int AtaqueFinal => base.AtaqueFinal + base.AtaqueFinal / 10;
-        public override int SacarVida => base.SacarVida - base.SacarVida / 10;
-
+        public byte DescontarVida(byte unaVida)
+        {
+            return (byte)(unaVida / 10);
+        }
     }
 }
